711481c79b19ba11a684f1ebb154f3cc4268c239
Mika Tejada <mikaylaaatejada@yahoo.com>
2021-09-04 09:15:39 +0800

    created chatroom ui

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -10,7 +10,22 @@ const homeController = {
 		}
 
 		res.render('homepage', dummyData)
-	}
+	},
+
+	getChatroom: function(req,res) {
+		var item = {
+			itemName: "ITEM",
+			itemDesc: "item's long-ish description",
+			startPrice: 100.00,
+			autobuyPrice: 1000.00,
+			bidTime: 15,
+			maxPeople: 10
+		}
+
+
+        res.render('chatroom', item);
+    },
+
 }
 
 module.exports = homeController
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const homeController = require('../controllers/homeController')
+//const loginController = require('../controllers/LoginController.js');
 
 const app = express();
 module.exports = app
 
 app.get('/', homeController.home);
+app.get('/chatroom', homeController.getChatroom);
